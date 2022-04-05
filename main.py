bb43d4feb15523ed76cdcba3d33fb8d5405189a0
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2022-04-05 20:32:17 +0500

    add help

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('5289190892:AAEizjNTS96yAz0rxdQiyKImRBWq4boLllo')
+TOKEN = '5289190892:AAEizjNTS96yAz0rxdQiyKImRBWq4boLllo'
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(info):
-    mess = info.from_user.first_name
-    markup = types.ReplyKeyboardRemove()
-    bot.send_message(info.chat.id, f'Привет, {mess}, я твой помощник!', reply_markup=markup)
+    name = info.from_user.first_name
+    bot.send_message(info.chat.id, f'Привет, {name}, я твой помощник!')
 
 
 @bot.message_handler(commands=['help'])
 def help(info):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    website = types.KeyboardButton('Веб сайт')
-    start_b = types.KeyboardButton('/start')
-    markup.add(website, start_b)
-    bot.send_message(info.chat.id, 'Что я могу', reply_markup=markup)
+    comms = open('help.txt', 'r', encoding="utf-8")
+    bot.send_message(info.chat.id, comms.read())
 
 
 bot.polling(none_stop=True)
