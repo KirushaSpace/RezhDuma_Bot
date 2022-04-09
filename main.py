a5f3a0fd35cbd34c2bafdbae91abdc587aefef77
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2022-04-09 22:51:06 +0500

    fix bugs

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def start(message):
     global persons
     name = message.from_user.first_name
     markup = types.InlineKeyboardMarkup(row_width=1)
-    buttons = [types.InlineKeyboardButton(text='Вход', callback_data='login'),
-               types.InlineKeyboardButton(text='Переход на сайт', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ')]
+    item_log = types.InlineKeyboardButton(text='Вход', callback_data='login')
+    item_site = types.InlineKeyboardButton(text='Переход на сайт', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     if message.from_user.id in persons:
-        buttons.append(types.InlineKeyboardButton(text='Личный кабинет', callback_data='lk'))
-    markup.add(buttons)
+        item_lk = types.InlineKeyboardButton(text='Личный кабинет', callback_data='lk')
+        markup.add(item_lk)
+    markup.add(item_log, item_site)
     bot.send_message(message.chat.id,
                      '\n'.join(map(lambda str: str.strip(), f'''Привет, {name}, я твой помощник!
                          пару слов туда-сюда
@@ -37,9 +38,9 @@ def answer(call):
         bot.register_next_step_handler(msg, start)
     elif call.data == 'lk':
         markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
-        buttons = [types.KeyboardButton(text='Почта'),
-                   types.KeyboardButton(text='заявки')]
-        markup.add(buttons)
+        item_mail = types.KeyboardButton(text='Почта')
+        item_zapr = types.KeyboardButton(text='заявки')
+        markup.add(item_mail, item_zapr)
         bot.send_message(call.message.chat.id, 'Выберете куда вы хотите перейти', reply_markup=markup)
 
 
@@ -52,18 +53,21 @@ def user_login(message):
     msg = bot.send_message(message.chat.id, 'Введите пароль:')
     bot.register_next_step_handler(msg, user_password)
 
-
 def user_password(message):
     password = message.text
     global person, persons
     person[1] = password
     if person not in persons:
-        persons.append([person[0], person[1], message.from_user.id])
+        persons.append(message.from_user.id)
     # todo обработчик пароля
     # todo проверка есть ли пользователь в системе
     bot.send_message(message.chat.id, 'Проверяю...')
     bot.send_message(message.chat.id, 'Успешный вход в систему, добро пожаловать, {Имя} {Фамилия}')
-    bot.send_message(message.chat.id, 'А все, тут функционал закончился')
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button = types.KeyboardButton('Вернуться')
+    markup.add(button)
+    msg = bot.send_message(message.chat.id, 'вернитесь в начало', reply_markup=markup)
+    bot.register_next_step_handler(msg, start)
 
 
 @bot.message_handler(commands=['help'])
@@ -76,7 +80,7 @@ def help(message):
 def per(message):
     global persons
     bot.send_message(message.chat.id,
-                     '\n'.join(map(lambda x: f'{x[0]} {x[1]} {x[2]}', persons)))
+                     '\n'.join(persons))
 
 
 @bot.message_handler(commands=['info'])
